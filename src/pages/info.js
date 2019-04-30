b47c924101d2b09bcd3562656608608a03668ec4
AndrewThian <andrewbobbythian@gmail.com>
2019-04-30 22:29:17 +0800

    finally fixed fade problem

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classNames from "classnames";
-import { useLaxElement } from "use-lax";
+import lax from "lax.js";
 
 import styles from "./info.module.css";
 
@@ -15,9 +15,23 @@ import { isMobile } from "../utils/CheckMobile";
 const WAIT_TIME = 100;
 const MULTIPLIER = 2;
 
-const FadeMobile = ({ children }) => {
-  const fadeInRef = useLaxElement();
+const Fade = ({ children, index }) => {
+  const fadeInRef = useRef();
+  useEffect(() => {
+    if (isMobile()) {
+      const currentNode = fadeInRef.current;
+      lax.addElement(currentNode);
+      return () => {
+        lax.removeElement(currentNode);
+      };
+    }
+  }, []);
 
+  const [showFade, setShowFade] = useState(false);
+  let animOffset = WAIT_TIME;
+  if (index > 0) {
+    animOffset = index * MULTIPLIER * WAIT_TIME;
+  }
   useEffect(() => {
     if (!isMobile()) {
       setTimeout(() => setShowFade(true), animOffset);
@@ -29,29 +43,6 @@ const FadeMobile = ({ children }) => {
       ref={fadeInRef}
       data-lax-opacity="(vh-100) 0, (vh-200) 1, (vh-300) 1"
       data-lax-anchor="self"
-      className={classNames(styles.column, styles.column__hide)}
-    >
-      {children}
-    </div>
-  );
-};
-
-const FadeDesktop = ({ children, index }) => {
-  const [showFade, setShowFade] = useState(false);
-
-  let animOffset = WAIT_TIME;
-  if (index > 0) {
-    animOffset = index * MULTIPLIER * WAIT_TIME;
-  }
-
-  useEffect(() => {
-    if (!isMobile()) {
-      setTimeout(() => setShowFade(true), animOffset);
-    }
-  }, []);
-
-  return (
-    <div
       className={classNames(styles.column, styles.column__hide, {
         [styles.column__show]: showFade,
       })}
@@ -61,44 +52,35 @@ const FadeDesktop = ({ children, index }) => {
   );
 };
 
-const FadeIn = ({ children, component }) =>
-  React.cloneElement(component, null, children);
+const COMPONENT_MAP = [
+  <>
+    <h1 className={styles.h1}>About</h1>
+    <About />
+  </>,
+  <>
+    <div className={styles.small__container}>
+      <h1 className={styles.h1}>Clients</h1>
+      <ClientsList />
+    </div>
+    <div>
+      <h1 className={styles.h1}>Education</h1>
+      <EduList />
+    </div>
+  </>,
+  <>
+    <h1 className={classNames(styles.h1, styles.info__title)}>Work</h1>
+    <WorkList />
+  </>,
+];
 
 const InfoPage = () => {
   return (
     <Layout className={styles.layout}>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div className={styles.columns}>
-        <FadeIn
-          component={isMobile() ? <FadeMobile /> : <FadeDesktop index={0} />}
-        >
-          <div>
-            <h1 className={styles.h1}>About</h1>
-            <About />
-          </div>
-        </FadeIn>
-        <FadeIn
-          component={isMobile() ? <FadeMobile /> : <FadeDesktop index={1} />}
-        >
-          <div>
-            <div className={styles.small__container}>
-              <h1 className={styles.h1}>Clients</h1>
-              <ClientsList />
-            </div>
-            <div>
-              <h1 className={styles.h1}>Education</h1>
-              <EduList />
-            </div>
-          </div>
-        </FadeIn>
-        <FadeIn
-          component={isMobile() ? <FadeMobile /> : <FadeDesktop index={2} />}
-        >
-          <div>
-            <h1 className={classNames(styles.h1, styles.info__title)}>Work</h1>
-            <WorkList />
-          </div>
-        </FadeIn>
+        {COMPONENT_MAP.map((component, index) => (
+          <Fade index={index}>{component}</Fade>
+        ))}
       </div>
     </Layout>
   );
