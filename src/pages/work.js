8f35e525fba4b69ef17944124d9ce637f96439a0
AndrewThian <andrewbobbythian@gmail.com>
2020-01-15 20:40:36 +0800

    add first rich text page

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,16 +2,40 @@ import React, { useState, useEffect, Fragment } from "react";
 import { graphql } from "gatsby";
 import classNames from "classnames";
 
+import { BLOCKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
 import Layout from "../components/layout/layout";
 import SEO from "../components/seo";
 
+const options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      return <p className={paragraphClass(node)}>{children}</p>;
+    },
+  },
+};
+
+function paragraphClass(node) {
+  const className = "odd";
+  //alternate logic for 'odd' | 'even'
+  return className;
+}
+
 const WorkPage = ({ data }) => {
-  console.log(data);
+  const {
+    detailPage: { content },
+  } = data;
+
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div>
         <span>still under construction</span>
+        {content.map(({ textNode: { richTextDocument } }) => {
+          console.log(richTextDocument);
+          return documentToReactComponents(richTextDocument, options);
+        })}
       </div>
     </Layout>
   );
@@ -21,15 +45,15 @@ export default WorkPage;
 
 export const query = graphql`
   query($slug: String!) {
-    contentfulDetailPage(metadata: { slug: { eq: $slug } }) {
+    detailPage: contentfulDetailPage(metadata: { slug: { eq: $slug } }) {
       id
       content {
         position
         contentType
         title
-        childContentfulDetailPageContentContentRichTextNode {
+        textNode: childContentfulDetailPageContentContentRichTextNode {
           content
-          json
+          richTextDocument: json
         }
       }
     }
